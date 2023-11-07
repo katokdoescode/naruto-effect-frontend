f7f4949b9e5f5ef89d44ad9c3755ed6698c7c0b2
katokdoescode <snakedoc.md@gmail.com>
2023-11-07 20:55:55 +0400

    Add styles for the button

--- src/lib/ui/Button.svelte
+++ src/lib/ui/Button.svelte
@@ -8,16 +8,27 @@
 	/** @type{"button" | "submit" | "reset" | null | undefined} type */
 	export let type = 'button';
 
-	/** @type {boolean} Will have border */
-	export let bordered = false;
-
 	/** @type {boolean} */
 	export let disabled = false;
 
 	/** @type {boolean} Will have active state styles */
 	export let active = false;
 
-	$: classes = `button ${bordered ? 'bordered' : ''} ${active ? 'active' : ''}`;
+	/** @type {"transparent"|"gray"|"black"|"green"|"red"|"white"} Color of the button */
+	export let color = 'transparent';
+
+	/** @type {"none"|"login"} Specific style of the button */
+	export let styleType = 'none';
+
+	$: classes = `button ${active ? 'active' : ''}`;
+
+	$: if (color !== 'transparent' && styleType === 'none') {
+		classes += 'bordered' + ' ' + color;
+	}
+
+	$: if (styleType !== 'none') {
+		classes += styleType;
+	}
 
 	function click(event) {
 		dispatch('click', event);
@@ -51,6 +62,53 @@
 		text-decoration: underline;
 		text-underline-offset: 3px;
 		text-decoration-thickness: 2px;
+	}
+
+	.button.bordered {
+		border: 2px solid var(--color-gray);
+		border-radius: 6px;
+		padding: 0 21px;
+		text-transform: lowercase;
+	}
+
+	.button.bordered.gray {
+		border-color: var(--color-gray);
+		background-color: var(--color-gray);
+		color: var(--color-black);
+	}
+
+	.button.bordered.green {
+		border-color: var(--color-green);
+		background-color: var(--color-green);
+		color: var(--color-black);
+	}
+
+	.button.bordered.red {
+		border-color: var(--color-red);
+		background-color: var(--color-red);
+		color: var(--color-black);
+	}
+
+	.button.bordered.black {
+		border-color: var(--color-main);
+		background-color: var(--color-main);
+		color: var(--color-bg-main);
+	}
+
+	.button.login {
+		border-radius: 6px;
+		border: none;
+		padding: 10px 23px;
+		background-color: var(--color-main);
+		color: var(--color-bg-main);
+	}
+
+	.button.bordered:hover {
+		filter: saturate(1.8);
+	}
+
+	.button.bordered:active {
+		filter: saturate(0.5);
 	}
 
 	.button:disabled {
