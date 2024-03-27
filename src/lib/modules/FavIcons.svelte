e629ad09a182018507d8c00c0e30f331880041af
katokdoescode <snakedoc.md@gmail.com>
2024-03-27 18:25:56 +0400

    Fix favicon locale name

--- src/lib/modules/FavIcons.svelte
+++ src/lib/modules/FavIcons.svelte
@@ -1,16 +1,19 @@
 <script>
 	import { ColorSchemes } from '$lib/constants';
 	import { locale } from 'svelte-i18n';
+	$: formattedLocale = $locale.split('-').length
+		? $locale.split('-')[0]
+		: $locale;
 </script>
 
 <svelte:head>
 	<link
-		href={`/ne-${$locale}-${ColorSchemes.DARK}.svg`}
+		href={`/ne-${formattedLocale}-${ColorSchemes.DARK}.svg`}
 		media="(prefers-color-scheme: light)"
 		rel="icon"
 	/>
 	<link
-		href={`/ne-${$locale}-${ColorSchemes.LIGHT}.svg`}
+		href={`/ne-${formattedLocale}-${ColorSchemes.LIGHT}.svg`}
 		media="(prefers-color-scheme: dark)"
 		rel="icon"
 	/>
