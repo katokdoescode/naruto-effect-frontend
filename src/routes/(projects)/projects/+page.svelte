a060f1c22b89e4001621c96212598aa93dcd0509
katokdoescode <snakedoc.md@gmail.com>
2024-07-29 16:19:04 +0200

    Create projects page

---
+++ src/routes/(projects)/projects/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+import { locale } from 'svelte-i18n';
+</script>
+
+<svelte:head>
+  {#if $locale === 'ru'}
+  <title>Проекты Эффекта Наруто</title>
+  {:else}
+  <title>Naruto Effect Projects</title>
+  {/if}
+</svelte:head>
+<nav>
+  {#if $locale === 'ru'}
+  <h1>Проекты Эффекта Наруто</h1>
+
+  <a href="/projects/lesnoe11">Лесное 11</a>
+  {:else}
+  <h1>Naruto Effect Projects</h1>
+
+  <a href="/projects/lesnoe11">Lesnoe 11</a>
+  {/if}
+</nav>
